4cbd4a671acb10cd0e6d1034044430dfcc20e2ed
Bekmurod <bek031291@mail.ru>
2021-09-18 13:41:25 +0500

    GetAmountById and SetAmountById

--- pkg/account/account_service.go
+++ pkg/account/account_service.go
@@ -19,32 +19,50 @@ func NewAccountServicce(connect *pgx.Conn) *AccountService{
 func (s *AccountService) GetAmountById(id int64) (int64, error) {
     var amount int64
     err:=s.connect.QueryRow(context.Background(),`select amount from account where id=$1`,id).Scan(&amount)
+	if err != nil {
+		utils.ErrCheck(err)
+		return amount,err
+	}
     return amount, err
 }
 func (s *AccountService) SetAmountById(amount,id int64)  error{
 	_,err:=s.connect.Exec(context.Background(),`update account set amount = $1 where id = $2`,amount,id)
-	utils.ErrCheck(err)
+	if err != nil {
+		utils.ErrCheck(err)
+		return err
+	}
 	return nil
 }
 
 //Перевод 
 func (s *AccountService) TransferMoneyByAccountId(payerAccountId,receiverAccountId int64, amount int64) error {
 	payerAmount,err:=s.GetAmountById(payerAccountId)
-	utils.ErrCheck(err)
+	if err != nil {
+		utils.ErrCheck(err)
+		return err
+	}
 	if amount > payerAmount {
 		log.Printf("не достаточно баланс")
 		return err
 	}
 	receiverAmount,err:=s.GetAmountById(receiverAccountId)
-	utils.ErrCheck(err)
-
+	if err != nil {
+		utils.ErrCheck(err)
+		return err
+	}
 	newPayerAmount:=payerAmount-amount
 	newreceiverAmount:=receiverAmount+amount
 
 	err=s.SetAmountById(newPayerAmount,payerAccountId)
-	utils.ErrCheck(err)
+	if err != nil {
+		utils.ErrCheck(err)
+		return err
+	}
 	err=s.SetAmountById(newreceiverAmount,receiverAccountId)
-	utils.ErrCheck(err)
+	if err != nil {
+		utils.ErrCheck(err)
+		return err
+	}
 	fmt.Println("Перевод Успешно отправлено!!!")
 	
 	return nil
